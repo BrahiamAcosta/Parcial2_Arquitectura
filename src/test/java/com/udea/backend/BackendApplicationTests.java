a6302e9ffed210406dc86210aadeb52d8b4f1628
BrahiamAcosta <brahiam.acosta@udea.edu.co>
2024-11-29 10:26:04 -0500

    initial commit

---
+++ src/test/java/com/udea/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udea.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
